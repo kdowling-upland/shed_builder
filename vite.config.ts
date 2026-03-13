984e9f403f471879f74084ef0a73d28377879cfd
Kevin Dowling <kdowling@uplandcapgroup.com>
2026-03-13 18:17:19 -0400

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/shed_builder/',
   plugins: [react(), tailwindcss()],
 })
